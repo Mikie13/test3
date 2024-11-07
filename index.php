f21f7208fbbe919b7869bbcbfbfb4d971d00726b
Mikie13 <mikiesaludar6@gmail.com>
2024-11-06 23:55:42 -0800

    index.php

--- index.php
+++ index.php
@@ -29,6 +29,24 @@
 	<input type="text" name="Lastname"class="form-control">
 </div>
 
+<div>
+	<label>Age</label>
+	<input type="text" name="Age"class="form-control">
+</div>
+
+<div>
+<label>Address</label>
+	<input type="text" name="Address"class="form-control">
+</div>
+
+<div>
+<label>Course&Section</label>
+	<input type="text" name="Course&Section"class="form-control">
+</div>
+
+
+<button type="submit"> </button>
+
 </html>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
